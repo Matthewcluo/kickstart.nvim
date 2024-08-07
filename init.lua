887fbadfb1eeee262213059c73a00394e6b97011
Matthew Luo <mattcluo@amazon.com>
2024-08-07 11:51:35 -0700

    init.lua: add quick (un)comment keybinds

--- init.lua
+++ init.lua
@@ -192,6 +192,41 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Keybinds for (un)commenting selected lines
+--   Use in either VISUAL or NORMAL mode
+--   refer to https://slar.se/comment-and-uncomment-code-in-neovim.html
+local non_c_line_comments_by_filetype = {
+  lua = '-- ',
+  python = '# ',
+  sql = '-- ',
+}
+
+local function comment_out(opts)
+  local line_comment = non_c_line_comments_by_filetype[vim.bo.filetype] or '// '
+  local start = math.min(opts.line1, opts.line2)
+  local finish = math.max(opts.line1, opts.line2)
+
+  vim.api.nvim_command(start .. ',' .. finish .. 's:^:' .. line_comment .. ':')
+  vim.api.nvim_command 'noh'
+end
+
+local function uncomment(opts)
+  local line_comment = non_c_line_comments_by_filetype[vim.bo.filetype] or '// '
+  local start = math.min(opts.line1, opts.line2)
+  local finish = math.max(opts.line1, opts.line2)
+
+  pcall(vim.api.nvim_command, start .. ',' .. finish .. 's:^\\(\\s\\{-\\}\\)' .. line_comment .. ':\\1:')
+  vim.api.nvim_command 'noh'
+end
+
+vim.api.nvim_create_user_command('CommentOut', comment_out, { range = true })
+vim.keymap.set('v', '<leader>co', ':CommentOut<CR>')
+vim.keymap.set('n', '<leader>co', ':CommentOut<CR>')
+
+vim.api.nvim_create_user_command('Uncomment', uncomment, { range = true })
+vim.keymap.set('v', '<leader>uc', ':Uncomment<CR>')
+vim.keymap.set('n', '<leader>uc', ':Uncomment<CR>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
