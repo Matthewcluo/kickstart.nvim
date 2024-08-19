97572758faed6f7a7c87666cf71be30038a5ba00
Matthew Luo <matthewluo@berkeley.edu>
2024-08-19 00:50:48 -0700

    install nvim-tree & disable neo-tree

---
+++ lua/kickstart/plugins/nvim-tree.lua
@@ -0,0 +1,72 @@
+-- nvim-tree plugin configuration
+-- refer to https://github.com/nvim-tree/nvim-tree.lua
+
+return {
+  'nvim-tree/nvim-tree.lua',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    local nvimtree = require 'nvim-tree'
+
+    -- recommended settings from nvim-tree documentation
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    nvimtree.setup {
+      view = {
+        width = 35,
+      },
+      -- change folder arrow icons
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          symlink_arrow = ' ➛ ',
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = true,
+          },
+          glyphs = {
+            git = {
+              unstaged = '✗',
+              staged = '✓',
+              unmerged = '',
+              renamed = 'R',
+              untracked = 'U',
+            },
+          },
+        },
+      },
+      -- disable window_picker for
+      -- explorer to work well with
+      -- window splits
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+      filters = {
+        custom = { '.DS_Store' },
+      },
+      git = {
+        ignore = false,
+      },
+    }
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    -- toggle file explorer
+    keymap.set('n', '<leader>ee', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
+    -- toggle file explorer on current file
+    keymap.set('n', '<leader>ef', '<cmd>NvimTreeFindFileToggle<CR>', { desc = 'Toggle file explorer on current file' })
+    -- collapse file explorer
+    keymap.set('n', '<leader>ec', '<cmd>NvimTreeCollapse<CR>', { desc = 'Collapse file explorer' })
+    -- refresh file explorer
+    keymap.set('n', '<leader>er', '<cmd>NvimTreeRefresh<CR>', { desc = 'Refresh file explorer' })
+  end,
+}
