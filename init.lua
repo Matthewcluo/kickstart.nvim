f98e52ed072539813a604058c25de604857a403d
Matthew Luo <matthewluo@berkeley.edu>
2025-02-25 12:08:37 -0800

    LSPs: comment out pyright and ocamllsp bc they arent working :(

--- init.lua
+++ init.lua
@@ -574,9 +574,9 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         gopls = {},
-        pyright = {},
+        -- pyright = {},
         rust_analyzer = {},
-        ocamllsp = {},
+        -- ocamllsp = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
