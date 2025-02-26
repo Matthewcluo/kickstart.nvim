6347fab65adc1dc8dc3cf693bdeefd00dbd5c813
Matthew Luo <matthewluo@berkeley.edu>
2025-02-26 13:22:20 -0800

    implement autocmd to close floaterm when exiting term mode
    Detects the exiting from Terminal mode with `TermLeave` and runs
    :Floaterminal to toggle (close) the floating terminal.

--- init.lua
+++ init.lua
@@ -203,6 +203,15 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
+-- Custom Autocommand for toggling (closing)
+-- the floating terminal when exiting terminal mode
+-- with <esc><esc> (double tap esc)
+vim.api.nvim_create_autocmd('TermLeave', {
+  callback = function()
+    vim.cmd 'Floaterminal'
+  end,
+})
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
